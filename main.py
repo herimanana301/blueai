415875eb1c5a8c3938bb24a94edaae7363511e28
Herimanana <herimanana301@gmail.com>
2023-05-23 23:10:58 +0200

    add first button

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ window.geometry("1080x720")
 window.iconbitmap("./src/icon.ico")
 window.config(background="#212121")
 
-titre = Label(window, text="Bienvenue sur BlueBrain", font=("Futura",40), bg=("#212121"), fg=("white"))
-titre.pack(expand=YES)
+titleContainer = Frame(window,bg="#212121")
+
+titre = Label(titleContainer, text="Bienvenue sur BlueBrain", font=("Futura",40), bg=("#212121"), fg=("white"))
+titre.pack()
 
 
+startButton = Button(titleContainer, text="Commencer", font=("Futura",20), bg=("#0074D9"), fg=("white"), relief="solid", borderwidth=0, highlightthickness=10)
+startButton.pack()
+titleContainer.pack(expand=YES)
 window.mainloop()
